a58436f1e5c9dbd669e819a530ba9292cfe937b7
Natchaya2540 <47807611+Natchaya2540@users.noreply.github.com>
2020-03-25 21:50:32 +0700

    add

--- app/Http/Controllers/ShowUserController.php
+++ app/Http/Controllers/ShowUserController.php
@@ -7,12 +7,12 @@ use Illuminate\Http\Request;
 class ShowUserController extends Controller
 {
     public function index(){
-        $id = Auth::user()->id;
-        $users= DB::table('users')
-            ->where('users.id',$id)
-            ->get();
-        return view('profile')
-            ->with('users',$id);
+//        $id = Auth::user()->id;
+//        $users= DB::table('users')
+//            ->where('users.id',$id)
+//            ->get();
+//        return view('profile')
+//            ->with('users',$id);
 
     }
 
